fc77b5953a94cce7a9c7f1c18c667a7d64b87498
Dmitry <dima124691@gmail.com>
2025-02-14 12:14:47 +0300

    adds input

--- internal/input/input.go
+++ internal/input/input.go
@@ -1,1 +1,25 @@
 package input
+
+import (
+	"image"
+	_ "image/jpeg"
+	"log"
+	"os"
+)
+
+func ReadImage(filepath string) image.Image {
+	reader, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bounds := m.Bounds()
+	log.Println(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y)
+
+	return m
+}
