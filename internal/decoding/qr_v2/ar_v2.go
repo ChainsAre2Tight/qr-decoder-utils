5d56f73cebae128432a074056ce4bd2daed9e860
ChainsAre2Tight <you@example.com>
2025-02-22 22:54:21 +0300

    refactor: makes qr v2 decoding inherited from v1

--- internal/decoding/qr_v2/ar_v2.go
+++ internal/decoding/qr_v2/ar_v2.go
@@ -1,10 +1,7 @@
 package qr_v2
 
 import (
-	"fmt"
-
-	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/decoding/common/data_formats"
-	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/decoding/common/masks"
+	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/decoding/qr_v1"
 	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/interfaces"
 	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/types"
 	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/utils"
@@ -31,36 +28,14 @@ func (QRVer2) OOB() interfaces.OutOfBoundsInterface {
 	return oob{}
 }
 
+// Inherited from QRVer1
 func (QRVer2) ReadMetadata(matrix [][]bool) (interfaces.ModeInterface, interfaces.MaskInterface, error) {
-	// omit first two bits, mode is not implemented
-	mode, err := utils.ReadMatrixRow(matrix, 8, 2, 5)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	mode, err = utils.XORSlices(mode, []bool{true, false, true})
-	if err != nil {
-		return nil, nil, err
-	}
-
-	modeString := utils.BoolSliceToString(mode)
-	mask, ok := masks.Masks[modeString]
-	if !ok {
-		return nil, nil, fmt.Errorf("no mask matches %s", modeString)
-	}
-
-	return nil, mask, nil
+	return qr_v1.QRVer1{}.ReadMetadata(matrix)
 }
 
+// Inherited from QRVer1
 func (QRVer2) ReadFormat(matrix [][]bool, mask interfaces.MaskInterface, reader interfaces.BitReaderInterface) (interfaces.FormatInterface, error) {
-	rawMetadata := reader.ReadMultiple(4)
-
-	metadataString := utils.BoolSliceToString(rawMetadata)
-	format, ok := data_formats.SUPPORTED_FORMATS[metadataString]
-	if !ok {
-		return nil, fmt.Errorf("format %s is unknown or is not implemented", metadataString)
-	}
-	return format, nil
+	return qr_v1.QRVer1{}.ReadFormat(matrix, mask, reader)
 }
 
 type oob struct{}
