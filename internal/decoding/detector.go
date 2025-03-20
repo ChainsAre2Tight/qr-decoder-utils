89ca48d290cefe5ac15fd4d9f2aae6b3c9b5feaa
dmitry <dmitry@abobotron>
2025-03-20 13:59:20 +0300

    makes qr detection a separate function and detectCodeType a controller

--- internal/decoding/detector.go
+++ internal/decoding/detector.go
@@ -9,19 +9,27 @@ import (
 	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/interfaces"
 )
 
-var KNOWN_CODES = []interfaces.CodeInterface{
+var QR_CODES = []interfaces.CodeInterface{
 	qr_v1.QRVer1{},
 	qr_v2.QRVer2{},
 	qr_v3.QRVer3{},
 }
 
-func detectCodeType(matrix [][]bool) (interfaces.CodeInterface, error) {
-
-	for _, code := range KNOWN_CODES {
+func detectQR(matrix [][]bool) (interfaces.CodeInterface, bool) {
+	for _, code := range QR_CODES {
 		ok := code.Detect(matrix)
 		if ok {
-			return code, nil
+			return code, true
 		}
+	}
+	return nil, false
+}
+
+func detectCodeType(matrix [][]bool) (interfaces.CodeInterface, error) {
+
+	code, ok := detectQR(matrix)
+	if ok {
+		return code, nil
 	}
 	return nil, fmt.Errorf("no known code types found")
 }
