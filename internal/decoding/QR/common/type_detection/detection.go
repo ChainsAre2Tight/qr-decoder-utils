87e8b410e3038225db9a9a7e026a23c18e19a24a
ChainsAre2Tight <you@example.com>
2025-03-22 18:21:45 +0300

    add docstring

--- internal/decoding/QR/common/type_detection/detection.go
+++ internal/decoding/QR/common/type_detection/detection.go
@@ -5,6 +5,9 @@ import (
 	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/interfaces"
 )
 
+// All hardcoded qr codes used in this app
+// Refer to table 1 for size parameter and
+// table E.1 for list of alignment patterns
 var QR_CODES = []interfaces.CodeInterface{
 	&qr.QR{Size: 21},
 	&qr.QR{Size: 25, AlignmentPatterns: []int{6, 18}},
