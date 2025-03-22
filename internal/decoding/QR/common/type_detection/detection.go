bf812e2bef9f6f48b4a2dd09e041617a8806cac4
ChainsAre2Tight <you@example.com>
2025-03-22 12:56:03 +0300

    switch to using universal QR type

--- internal/decoding/QR/common/type_detection/detection.go
+++ internal/decoding/QR/common/type_detection/detection.go
@@ -1,16 +1,14 @@
 package type_detection
 
 import (
-	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/decoding/QR/qr_v1"
-	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/decoding/QR/qr_v2"
-	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/decoding/QR/qr_v3"
+	qr "github.com/ChainsAre2Tight/qr-decoder-utils/internal/decoding/QR"
 	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/interfaces"
 )
 
 var QR_CODES = []interfaces.CodeInterface{
-	qr_v1.QRVer1{},
-	qr_v2.QRVer2{},
-	qr_v3.QRVer3{},
+	&qr.QR{Size: 21},
+	&qr.QR{Size: 25, AlignmentPatterns: [][2]int{{16, 16}}},
+	&qr.QR{Size: 29, AlignmentPatterns: [][2]int{{20, 20}}},
 }
 
 func DetectQR(matrix [][]bool) (interfaces.CodeInterface, bool) {
