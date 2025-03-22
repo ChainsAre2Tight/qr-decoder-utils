8385952bcc68ab940f2f834aff17667374d35880
ChainsAre2Tight <you@example.com>
2025-03-22 19:50:06 +0300

    removes redundancy

--- internal/decoding/QR/common/type_detection/detection.go
+++ internal/decoding/QR/common/type_detection/detection.go
@@ -9,10 +9,10 @@ import (
 // Refer to table 3 for content length parameters and
 // table E.1 for list of alignment patterns
 var QR_CODES = []*qr.QR{
-	&qr.QR{Name: "QR Version 1", Size: 21, Cci: qr.CCI1dash9, AlignmentPatterns: []int{}},
-	&qr.QR{Name: "QR Version 2", Size: 25, Cci: qr.CCI1dash9, AlignmentPatterns: []int{6, 18}},
-	&qr.QR{Name: "QR Version 3", Size: 29, Cci: qr.CCI1dash9, AlignmentPatterns: []int{6, 22}},
-	&qr.QR{Name: "QR Version 4", Size: 33, Cci: qr.CCI1dash9, AlignmentPatterns: []int{6, 26}},
+	{Name: "QR Version 1", Size: 21, Cci: qr.CCI1dash9, AlignmentPatterns: []int{}},
+	{Name: "QR Version 2", Size: 25, Cci: qr.CCI1dash9, AlignmentPatterns: []int{6, 18}},
+	{Name: "QR Version 3", Size: 29, Cci: qr.CCI1dash9, AlignmentPatterns: []int{6, 22}},
+	{Name: "QR Version 4", Size: 33, Cci: qr.CCI1dash9, AlignmentPatterns: []int{6, 26}},
 }
 
 func DetectQR(matrix [][]bool) (*qr.QR, bool) {
