2b10d78ec16a5c80a7c60867e79496d4c65f7cae
ChainsAre2Tight <you@example.com>
2025-04-06 14:06:23 +0300

    adds e2e tests for ECC200 datamatrixes

--- internal/cli/utils.go
+++ internal/cli/utils.go
@@ -8,14 +8,14 @@ import (
 	"github.com/ChainsAre2Tight/qr-decoder-utils/internal/input"
 )
 
-func LoadAndConvert(inputFilenamePtr *string) [][]bool {
+func LoadAndConvert(inputFilenamePtr *string, size int) [][]bool {
 
 	// load image
 	img := input.ReadImage(*inputFilenamePtr)
 
 	// detect borders and resize
 	validateOutputSize()
-	qr, err := input.DetectCodeOnImage(img, *outputSizePtr)
+	qr, err := input.DetectCodeOnImage(img, size)
 	if err != nil {
 		log.Fatal(err)
 	}
